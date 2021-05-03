162e3bc801ada40388b8e1059554d84e20177125
Srujan Deshpande <srujan.deshpande@gmail.com>
2021-05-04 01:22:38 +0530

    Added parsing and discord hook code

--- main.py
+++ main.py
@@ -2,6 +2,11 @@ import requests
 from datetime import datetime
 import pytz
 import copy
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 
 tz = pytz.timezone("Asia/Calcutta")
 currdate = datetime.now(tz).strftime("%d-%m-%Y")
@@ -16,15 +21,33 @@ r = requests.get(url=URL)
 # extracting data in json format
 data = r.json()
 
-res = []
+res = ""
 for center in data["centers"]:
     center_copy = copy.deepcopy(center)
     center_copy.pop("sessions")
     center_copy["sessions"] = []
     for session in center["sessions"]:
         if (session["min_age_limit"] == 18) and (session["available_capacity"] != 0):
-            center_copy["sessions"].append(session)
+            session_copy = copy.deepcopy(session)
+            session_copy.pop("slots")
+            session_copy.pop("session_id")
+            center_copy["sessions"].append(session_copy)
     if len(center_copy["sessions"]) != 0:
-        res.append(center_copy)
+        res += str(center_copy)
+        res += "\n\n"
 
-print(len(res))
+url = os.getenv("DISCORD_HOOK")
+
+if len(res) != 0:
+    res = res.replace("'", "")
+    res = res.replace('"', "")
+    content = "**Vaccine Stock Detected! -> *" + timestamp + "***\n" + str(res)
+
+    split_strings = []
+    n = 2000
+    for index in range(0, len(content), n):
+        split_strings.append(content[index : index + n])
+
+    for i in split_strings:
+        obj = {"content": i}
+        requests.post(url, data=obj)
